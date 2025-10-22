14f9e320988819582f8917ef631a5d2058627bfa
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2025-10-22 20:23:02 +0530

    feat: implemented function to display fetched data
    - to display fetched data on the page
    - updated the processedLocationData to remove the units from some properties

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let apiKey = "7K26VXR2V2A5L6A5KKMD42T4X";
 const locationInput = document.querySelector("#location");
 const unitGroupSwitchBtn = document.querySelector(".unitGroupSwitch");
 const weatherForm = document.querySelector(".weatherForm");
+const weatherDetailsContainer = document.querySelector(".locationWeatherDetails");
 
 async function getWeather(location, unitGroup) {
     let locationData;
@@ -14,8 +15,10 @@ async function getWeather(location, unitGroup) {
     let responseJson = await locationData.json(); 
     let locationWeatherDetails = processLocationData(responseJson);
 
-    console.log(locationWeatherDetails);
-    console.log(responseJson);
+    addToDom(locationWeatherDetails);
+
+    // console.log(locationWeatherDetails);
+    // console.log(responseJson);
 }
 
 function processLocationData(jsonResponse){
@@ -25,9 +28,8 @@ function processLocationData(jsonResponse){
         temp: jsonResponse.currentConditions.temp,
         feelsLike: jsonResponse.currentConditions.feelslike,
         humidity: jsonResponse.currentConditions.humidity,
-        visibility: `${jsonResponse.currentConditions.visibility} km`,
-        windSpeed: `${jsonResponse.currentConditions.windspeed} km`,
-
+        visibility: jsonResponse.currentConditions.visibility,
+        windSpeed: jsonResponse.currentConditions.windspeed,
     }
 
     return weatherDetails;
@@ -39,3 +41,40 @@ weatherForm.addEventListener("submit", (e) => {
     let currentUnit = unitGroupSwitchBtn.value;
     getWeather(inputLocation, currentUnit);
 })
+
+function addToDom(processedJson){
+    let container = document.createElement("div");
+
+    let cityName = document.createElement("h2");
+    cityName.textContent = locationInput.value;
+
+    let currentDate = document.createElement("p");
+    currentDate.textContent = processedJson.dateTime;
+
+    let currentCondition = document.createElement("p");
+    currentCondition.textContent = processedJson.condition;
+
+    let currentTemp = document.createElement("p");
+    let currentFeelsLike = document.createElement("p");
+
+    let currentHumidity = document.createElement("p");
+    currentHumidity.textContent = `${processedJson.humidity} %`;
+
+    let currentVisibility = document.createElement("p");
+    let currentWindSpeed = document.createElement("p");
+
+    if(unitGroupSwitchBtn.value === "C"){
+        currentTemp.textContent = `${processedJson.temp} °C`;
+        currentFeelsLike.textContent = `${processedJson.feelsLike} °C`;
+        currentVisibility.textContent = `${processedJson.visibility} km`;
+        currentWindSpeed.textContent = `${processedJson.windSpeed} km`;
+    }else {
+        currentTemp.textContent = `${processedJson.temp} °F`;
+        currentFeelsLike.textContent = `${processedJson.feelsLike} °F`;
+        currentVisibility.textContent = `${processedJson.visibility} mi`;
+        currentWindSpeed.textContent = `${processedJson.windSpeed} mph`;
+    }
+
+    container.append(cityName, currentDate, currentCondition, currentTemp, currentFeelsLike, currentHumidity, currentVisibility, currentWindSpeed);
+    weatherDetailsContainer.appendChild(container);
+}
